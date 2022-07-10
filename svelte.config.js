ff7823df0a5392a2a972051de50ab6333f93ccb2
Uğur Korkmaz <korkmaz.uu@gmail.com>
2022-07-11 00:12:57 +0300

    pages/docs

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,17 @@
 import adapter from '@sveltejs/adapter-static';
-
+const dev = "production" === "development";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
+			pages: 'docs/',
+			assets: 'docs/',
 			fallback: null,
 			precompress: false
 		}),
+		paths: {
+			base: dev ? "" : "/isvg.github.io",
+		},
 		prerender: {
 			default: true
 		}
